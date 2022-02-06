57d17d89ecdfdb2df5ef53e4dba9a34a5f432f8c
Roberto Linares <blendboy@gmail.com>
2022-02-06 01:35:21 -0600

    Configuracion de slf4j-simple para el logging interno de SparkJava y log4j2 para el logging de la aplicacion.

--- src/main/java/org/robertux/wordleCheater/Main.java
+++ src/main/java/org/robertux/wordleCheater/Main.java
@@ -2,11 +2,19 @@ package org.robertux.wordleCheater;
 
 import static spark.Spark.*;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+	public static String DEFAULT_PORT = "8888";
+	private static Logger logger = LogManager.getLogger(Main.class);
+	
 	public static void main(String[] args) {
-		port(Integer.parseInt(System.getProperty("port")));
+		logger.debug("Setting port {}", System.getProperty("port", DEFAULT_PORT));
+		port(Integer.parseInt(System.getProperty("port", DEFAULT_PORT)));
 		
 		get("/query/:name", (req, res) -> {
+			logger.debug("Query for partial word {}", req.params(":name"));
 			return "Hello: " + req.params(":name");
 		});
 	}

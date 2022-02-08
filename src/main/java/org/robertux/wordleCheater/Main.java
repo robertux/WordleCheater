bdec28d6d35276d156aa37860a464e6a775fa26f
Roberto Linares <blendboy@gmail.com>
2022-02-08 01:03:47 -0600

    Adición de soporte de diccionarios en inglés y español en base a paŕametro de lenguaje

--- src/main/java/org/robertux/wordleCheater/Main.java
+++ src/main/java/org/robertux/wordleCheater/Main.java
@@ -1,25 +1,46 @@
 package org.robertux.wordleCheater;
 
-import static spark.Spark.*;
+import static spark.Spark.get;
+import static spark.Spark.port;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.eclipse.jetty.http.HttpStatus;
+
+import com.google.gson.Gson;
 
 public class Main {
 	public static String DEFAULT_PORT = "8888";
+	public static String SPANISH_DICTIONARY_URL = "https://robertux-words.s3.amazonaws.com/es.txt";
+	public static String ENGLISH_DICTIONARY_URL = "https://robertux-words.s3.amazonaws.com/en.txt";
 	private static Logger logger = LogManager.getLogger(Main.class);
 	
 	public static void main(String[] args) {
+		Gson gson = new Gson();
+		
 		logger.debug("Setting port {}", System.getProperty("port", DEFAULT_PORT));
 		port(Integer.parseInt(System.getProperty("port", DEFAULT_PORT)));
 		
-		get("/find/:name", (req, res) -> {
-			logger.debug("Query for partial word {}", req.params(":name"));
-			WordFinder finder = new WordFinder(Arrays.asList(new String[] {"angel", "ansel", "aseno", "aesco", "cerco"}));
+		Map<String, WordFinder> finders = new HashMap<>();
+		finders.put("es", new WordFinder(DictionaryFecther.fetch(SPANISH_DICTIONARY_URL)));
+		finders.put("en", new WordFinder(DictionaryFecther.fetch(ENGLISH_DICTIONARY_URL)));
+		
+		get("/find/:lang/:word", (req, res) -> {
+			String lang = req.params(":lang");
+			String word = req.params(":word");
+			logger.debug("Query for partial word {} in language", word, lang);
+			
+			if (!finders.containsKey(lang)) {
+				res.status(HttpStatus.NOT_FOUND_404);
+				res.type("text/plain");
+				return "Language not found";
+			}
+			
 			res.type("application/json");
-			return Arrays.toString(finder.find(new Word(req.params(":word"))).toArray());
+			return gson.toJson(finders.get(lang).find(new Word(word)));
 		});
 	}
 }
